2d791209dc13a78a46805ad2b58e206a1eb87bf8
Aniedi Ubokudom <aniedi.anthonyu@yahoo.com>
2023-09-26 21:07:46 -0400

    added veggies

---
+++ models/vegetables.js
@@ -0,0 +1,31 @@
+const vegetables = [
+    {
+      name: "spinach",
+      color: "green",
+      readyToEat: true,
+    },
+    {
+      name: "tomato",
+      color: "red",
+      readyToEat: true,
+    },
+    {
+      name: "pepper",
+      color: "red",
+      readyToEat: true,
+    },
+    {
+      name: "cabbage",
+      color: "purple",
+      readyToEat: false,
+    },
+    {
+      name: "bean sprout",
+      color: "white",
+      readyToEat: true,
+    },
+  ];
+  
+  
+  //We must tell javascript which variable we want to be the result of the require()statement in server.js
+  module.exports = vegetables;
--- server.js
+++ server.js
@@ -1,60 +1,87 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-//data
-const fruits=require('./models/fruits') //require the new fruits.js file in server.js
-
-
-//An array of fruits
-// const fruits = ['apple', 'banana', 'pear'];
-
-//As data keeps growing, they could get messy, we move them to dedicated folders. 
-// Move this KVP to a fruits.js file under a modules folder
-//Fruits-key value pairs/Object
-// const fruits = [
-//     {
-//         name:'apple',
-//         color: 'red',
-//         readyToEat: true
-//     },
-//     {
-//         name:'pear',
-//         color: 'green',
-//         readyToEat: false
-//     },
-//     {
-//         name:'banana',
-//         color: 'green',
-//         readyToEat: false
-//     },
-//     {
-//         name:'kamquat',
-//         color: 'orange',
-//         readyToEat: true
-//     },
-//     {
-//         name:'lychee',
-//         color: 'fuschia',
-//         readyToEat: true
-//     },
-// ];
-
-//add our view template above the routes
+const jsxEngine = require("jsx-view-engine");
+const fruits = require("./models/fruits.js"); // start with ./ if it's just a file, not an NPM package
+const vegetables = require("./models/vegetables.js");
 
 
-//routes
-//index route-All the fruits
-app.get('/fruits/', (req, res) => {
-    res.send(fruits);
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine());
+
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine());
+
+//near the top, around other app.use() calls
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
 });
 
-//add show route- show singular route
-app.get('/fruits/:indexOfFruitsArray', (req, res) => {
-    res.send(fruits[req.params.indexOfFruitsArray]);
-    // res.render('show')//renders the info using the appropriate template
+// this is where I CRUD
+
+app.get("/fruits/", (req, res) => {
+  // res.send(fruits);
+  res.render('fruits/Index', { fruits: fruits });
+  res.render("fruits/Index", { fruits: fruits });
 });
 
-
-app.listen(3000, () => {
-    console.log('listening');
+app.get("/vegetables/", (req, res) => {
+  res.render('vegetables/Index', { vegetables: vegetables });
 });
+
+// new fruits
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/New");
+});
+//new veggies
+app.get("/vegetables/new", (req, res) => {
+  res.render("vegetables/New");
+});
+
+// CRUD
+// CRUD
+// CRUD
+app.post("/fruits", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true; //do some data correction
+  } else {
+    //if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false; //do some data correction
+  }
+  fruits.push(req.body);
+  res.redirect('/fruits'); //send the user back to /fruits
+});
+
+app.post("/vegetables", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+  vegetables.push(req.body);
+  res.redirect('/vegetables');
+});
+
+//add show route
+app.get("/fruits/:indexOfFruitsArray", (req, res) => {
+    // res.send(fruits[req.params.indexOfFruitsArray]);
+    res.render("fruits/Show", {
+      //second param must be an object
+      fruit: fruits[req.params.indexOfFruitsArray], //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.indexOfFruitsArray]
+    });
+  });
+  app.get("/vegetables/:indexOfVegetablesArray", (req, res) => {
+    res.render("vegetables/Show", {
+      vegetable: vegetables[req.params.indexOfVegetablesArray],
+    });
+  });
+
+
+
+  
+  app.listen(3000, () => {
+    console.log("listening");
+  });
